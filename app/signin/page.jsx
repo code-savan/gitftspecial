ef2211b318047d629c551c31d0fd0a70d98f3f19
code-savan <eric.marvelboy@gmail.com>
2024-12-24 14:23:52 +0100

    fixed login pages

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -4,20 +4,28 @@ import { signIn } from "next-auth/react"
 import { redirect } from "next/navigation"
 import { useState } from "react"
 import Link from "next/link"
+import { AtSign, Eye, EyeOff, KeyRound, Mail } from "lucide-react"
 
 export default function SignIn() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+     const [loading, setLoading] = useState(false)
+
+    const reveal = async () => {
+        setShowPassword(!showPassword)
+    }
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+      e.preventDefault()
+      setLoading(true)
     const result = await signIn("credentials", {
       email,
       password,
       redirect: false,
     })
-
+    setLoading(false)
     if (result?.error) {
       setError("Invalid email or password")
     } else {
@@ -27,37 +35,49 @@ export default function SignIn() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
-        <h1 className="text-3xl font-bold mb-6 text-center">Sign In</h1>
-        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="w-full md:max-w-md rounded-lg shadow-lg md:p-8 p-5 bg-[#03081b]">
+        <h1 className="text-2xl font-bold mb-4 text-center text-slate-200">Welcome Back</h1>
+        {error && <p className="text-red-500 text-center mb-4 text-[14px] font-semibold">{error}</p>}
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+          <div className="relative">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-300">Email</label>
             <input
               id="email"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-                          required
-                          placeholder="example@gmail.com"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-            />
+              required
+              placeholder="example@gmail.com"
+              className="mt-1 block w-full px-3 py-2  bg-transparent border border-slate-700 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 pl-[40px]"
+                      />
+                      <Mail className="absolute bottom-[10px] left-3" size={20} color="#94a3b8" />
           </div>
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+          <div className="relative">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-300">Password</label>
             <input
               id="password"
-              type="password"
+              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
                           required
                           placeholder="***********"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-            />
+              className="mt-1 block w-full py-2 bg-transparent border border-slate-700 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 px-[40px]"
+                      />
+                      <KeyRound className="absolute bottom-[10px] left-2" size={20} color="#94a3b8" />
+                      {
+                          showPassword ?
+                          <Eye className="absolute bottom-[10px] right-3 cursor-pointer" size={20} color="#94a3b8" onClick={reveal} />
+                          :
+                          <EyeOff className="absolute bottom-[10px] right-3 cursor-pointer" size={20} color="#94a3b8" onClick={reveal} />
+
+                }
           </div>
-          <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-            Sign In
+                  <button type="submit" disabled={loading ? true : false} className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                      {
+                          loading ? "Loading..." : " Sign In"
+                      }
+
           </button>
         </form>
         <p className="mt-4 text-center text-sm text-gray-600">
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import { redirect } from "next/navigation"
+import { AtSign, Eye, EyeOff, KeyRound, Mail, User } from "lucide-react"
 
 export default function SignUp() {
 
@@ -11,7 +12,13 @@ export default function SignUp() {
   const [password, setPassword] = useState("")
   const [username, setUsername] = useState("")
   const [error, setError] = useState("")
-  const [ref, setRef] = useState("")
+const [ref, setRef] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const reveal = async () => {
+        setShowPassword(!showPassword)
+    }
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search)
@@ -22,7 +29,8 @@ export default function SignUp() {
   }, [])
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+      e.preventDefault()
+      setLoading(true)
     const response = await fetch("/api/auth/signup", {
       method: "POST",
       headers: {
@@ -30,6 +38,7 @@ export default function SignUp() {
       },
       body: JSON.stringify({ name, email, password, username, ref }),
     })
+      setLoading(false)
 
     if (response.ok) {
 
@@ -69,13 +78,13 @@ export default function SignUp() {
 
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
-        <h1 className="text-3xl font-bold mb-6 text-center">Sign Up</h1>
-        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="w-full md:max-w-md rounded-lg shadow-lg md:p-8 p-5 bg-[#03081b]">
+        <h1 className="text-2xl font-bold mb-4 text-center text-slate-200">Create an Account</h1>
+        {error && <p className="text-red-500 text-center mb-4 text-[14px] font-semibold">{error}</p>}
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+          <div className="relative">
+            <label htmlFor="name" className="block text-sm font-medium text-gray-300">Name</label>
             <input
               id="name"
               type="text"
@@ -83,12 +92,13 @@ export default function SignUp() {
               onChange={(e) => setName(e.target.value)}
                           required
                            placeholder="John Doe"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm  placeholder-gray-400 text-slate-600
-                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-            />
+              className="mt-1 block w-full px-3 py-2  bg-transparent border border-slate-700 rounded-md text-sm shadow-sm  placeholder-gray-400 text-slate-600
+                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 pl-[40px]"
+                      />
+            <User className="absolute bottom-[10px] left-3" size={20} color="#94a3b8" />
           </div>
-          <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+          <div className="relative">
+            <label htmlFor="username" className="block text-sm font-medium text-gray-300">Username</label>
             <input
               id="username"
               type="text"
@@ -96,12 +106,13 @@ export default function SignUp() {
               onChange={(e) => setUsername(e.target.value)}
                           required
                            placeholder="johndoe"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600
-                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-            />
+              className="mt-1 block w-full px-3 py-2  bg-transparent border border-slate-700 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600
+                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 pl-[40px]"
+                      />
+                      <AtSign className="absolute bottom-[10px] left-3" size={20} color="#94a3b8" />
           </div>
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+          <div className="relative">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-300">Email</label>
             <input
               id="email"
               type="email"
@@ -109,25 +120,35 @@ export default function SignUp() {
               onChange={(e) => setEmail(e.target.value)}
                           required
                           placeholder="example@gmail.com"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600
-                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-            />
+              className="mt-1 block w-full px-3 py-2  bg-transparent border border-slate-700 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600
+                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 pl-[40px]"
+                      />
+                      <Mail className="absolute bottom-[10px] left-3" size={20} color="#94a3b8" />
           </div>
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+          <div className="relative">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-300">Password</label>
             <input
               id="password"
-              type="password"
+              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
                           required
                           placeholder="***********"
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600
-                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-            />
+              className="mt-1 block w-full py-2  bg-transparent border border-slate-700 rounded-md text-sm shadow-sm placeholder-gray-400 text-slate-600
+                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 px-[40px]"
+                      />
+                      <KeyRound className="absolute bottom-[10px] left-3" size={20} color="#94a3b8" />
+                         {
+                          showPassword ?
+                          <Eye className="absolute bottom-[10px] right-3 cursor-pointer" size={20} color="#94a3b8" onClick={reveal} />
+                          :
+                          <EyeOff className="absolute bottom-[10px] right-3 cursor-pointer" size={20} color="#94a3b8" onClick={reveal} />
+                }
           </div>
-          <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
-            Sign Up
+          <button type="submit"  disabled={loading ? true : false} className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+           {
+               loading ? "Loading..." : "Sign Up"
+           }
           </button>
         </form>
         <p className="mt-4 text-center text-sm text-gray-600">
